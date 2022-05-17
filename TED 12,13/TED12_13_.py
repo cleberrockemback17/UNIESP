4a25660eb821749a7cd448d9a22beea1579018a6
cleberrockemback17 <103398202+cleberrockemback17@users.noreply.github.com>
2022-05-17 20:34:58 -0300

    Add files via upload

---
+++ TED 12,13/TED12_13_.py
@@ -0,0 +1,10 @@
+dicionario = {'Phyton' : 'Linguagem de Programação', 
+            'HTML' : 'Liguagem de Marcação',
+            'Algoritimo' : 'Sequencia de passos para um objetivo',
+            'Messias' : 'Professor de Phyton',
+            'Visual Studio Code' : 'Editor de código-fonte'
+}
+
+for l in dicionario:
+    print(f'A palavra é {l} e o seu significado é {dicionario[l]}')
+    print('-' * 90)
